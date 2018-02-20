4caa213c08deb5f9d8edfae723d8b74070901986
Cody Selman <codyleeselman@gmail.com>
2018-02-20 17:14:42 -0500

    Created 4x4 grid and inserted a button into each cell.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+
+    };
+    //binds
+  }
   render() {
     return (
-      <h1>hello</h1>
+      <div>
+        <div className='container'>
+          <div className='row'>
+          <div className="col-3"><button>1</button></div>
+          <div className="col-3"><button>2</button></div>
+          <div className="col-3"><button>3</button></div>
+          <div className="col-3"><button>4</button></div>
+          <div className="col-3"><button>5</button></div>
+          <div className="col-3"><button>6</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          <div className="col-3"><button>0</button></div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
