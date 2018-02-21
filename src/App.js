694d4a7d9f4d348f6fd9776ca99707126146a77f
Cody Selman <codyleeselman@gmail.com>
2018-02-20 20:16:30 -0500

    Began making function to map Cells to dom.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Cell from './Cell.js';
 
 class App extends Component {
   constructor(){
@@ -15,32 +16,19 @@ class App extends Component {
   gameStart(){
     let newCells = [];
     for(let i=0;i<15;i+=1){
-      newCells.push('hello');
-      
+      newCells.push(Cell);
     }
-    console.log(newCells);
+    this.setState({
+      cells: newCells
+    });
   }
   render() {
     return (
       <div>
+        <h1 className='text-center'>React Fifteen</h1>
         <div className='container'>
           <div className='row'>
-          <div className="col-3"><button className='w-100'>1</button></div>
-          <div className="col-3"><button className='w-100'>2</button></div>
-          <div className="col-3"><button className='w-100'>3</button></div>
-          <div className="col-3"><button className='w-100'>4</button></div>
-          <div className="col-3"><button className='w-100'>5</button></div>
-          <div className="col-3"><button className='w-100'>6</button></div>
-          <div className="col-3"><button className='w-100'>7</button></div>
-          <div className="col-3"><button className='w-100'>8</button></div>
-          <div className="col-3"><button className='w-100'>9</button></div>
-          <div className="col-3"><button className='w-100'>10</button></div>
-          <div className="col-3"><button className='w-100'>11</button></div>
-          <div className="col-3"><button className='w-100'>12</button></div>
-          <div className="col-3"><button className='w-100'>13</button></div>
-          <div className="col-3"><button className='w-100'>14</button></div>
-          <div className="col-3"><button className='w-100'>15</button></div>
-          <div className="col-3"><button className='w-100'>-</button></div>
+          {this.state.cells}
           </div>
         </div>
       </div>
--- src/Cell.js
+++ src/Cell.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Cell = props => {
     return(
-        <div className="col-3"><button className='w-100'>1</button></div>
+        <div onClick={()=>console.log('clicked')} className="col-3">
+            <button className='w-100'>1</button>
+        </div>
     );
 };
 
