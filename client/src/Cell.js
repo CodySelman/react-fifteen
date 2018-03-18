213f2b3fcca4fff3a6a79da85d7778a700351612
Cody Selman <codyleeselman@gmail.com>
2018-03-18 00:20:20 -0400

    In Cell component, logic added that ensures blank cell does not have background image. Cleaned up and reorganized code in Cell component.

--- client/src/Cell.js
+++ client/src/Cell.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import Proptypes from 'prop-types';
-import './Cell.css';
+import React from "react";
+import Proptypes from "prop-types";
+import "./Cell.css";
 
 const Cell = props => {
-    const size = 100 / props.gridSize;
-    const columnNum = (props.value - 1) % props.gridSize;
-    const rowNum = Math.floor( (props.value - 1) / props.gridSize);
-    //hard coding some values temporarily like image size 500
-    const left = columnNum * -500/3  ;
-    const top = rowNum * -500/3;
-    const style = {
-        width: size + '%',
-        height: size + '%',
-        background: `url(https://lmcdesign-rj6zcy7b8ypu79snuv.netdna-ssl.com/wp-content/uploads/2016/01/Shrek_Tile-1-500x500.jpg) ${left}px ${top}px`
-    }
-    const blankCellValue = Math.pow(props.gridSize, 2);
-    return(
-        <button className=
-                {props.value === blankCellValue ? 
-                "Cell--cell Cell--cell-blank" : 
-                'Cell--cell'}
-                onClick={props.onClick}
-                style={style}>
-            {props.value === blankCellValue ? '' : props.value}
-        </button>
-    );
+  const gridSize = props.gridSize;
+  const size = 100 / gridSize;
+  const blankCellValue = Math.pow(gridSize, 2);
+  const columnNum = (props.value - 1) % gridSize;
+  const rowNum = Math.floor((props.value - 1) / gridSize);
+  //hard coding some values temporarily like image size 500
+  const left = columnNum * -500 / gridSize;
+  const top = rowNum * -500 / gridSize;
+
+  const style = {
+    width: size + "%",
+    height: size + "%",
+    background:
+      props.value === blankCellValue
+        ? ""
+        : `url(https://lmcdesign-rj6zcy7b8ypu79snuv.netdna-ssl.com/wp-content/uploads/2016/01/Shrek_Tile-1-500x500.jpg) ${left}px ${top}px`
+  };
+  return (
+    <button
+      className={
+        props.value === blankCellValue
+          ? "Cell--cell Cell--cell-blank"
+          : "Cell--cell"
+      }
+      onClick={props.onClick}
+      style={style}
+    >
+      {props.value === blankCellValue ? "" : props.value}
+    </button>
+  );
 };
 
 Cell.Proptypes = {
-    value: Proptypes.number.isRequired,
-    onClick: Proptypes.func.isRequired,
-    gridSize: Proptypes.number.isRequired
-}
+  value: Proptypes.number.isRequired,
+  onClick: Proptypes.func.isRequired,
+  gridSize: Proptypes.number.isRequired
+};
 
 export default Cell;
