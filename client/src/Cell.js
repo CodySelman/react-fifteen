d93821c83bcd245d56f5408c162ea5b31c7964e3
Cody Selman <codyleeselman@gmail.com>
2018-03-18 00:13:01 -0400

    Cell component now uses background image as background for puzzle pieces.

--- client/src/Cell.js
+++ client/src/Cell.js
@@ -4,9 +4,15 @@ import './Cell.css';
 
 const Cell = props => {
     const size = 100 / props.gridSize;
+    const columnNum = (props.value - 1) % props.gridSize;
+    const rowNum = Math.floor( (props.value - 1) / props.gridSize);
+    //hard coding some values temporarily like image size 500
+    const left = columnNum * -500/3  ;
+    const top = rowNum * -500/3;
     const style = {
         width: size + '%',
-        height: size + '%'
+        height: size + '%',
+        background: `url(https://lmcdesign-rj6zcy7b8ypu79snuv.netdna-ssl.com/wp-content/uploads/2016/01/Shrek_Tile-1-500x500.jpg) ${left}px ${top}px`
     }
     const blankCellValue = Math.pow(props.gridSize, 2);
     return(
