84dbb58a8b5fb25af19519039798de0c4a76f109
Cody Selman <codyleeselman@gmail.com>
2018-03-31 17:30:42 -0400

    Corrected bug regarding handleClick calling slideUp and slideDown method conditionals.

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      sizeRow: 2,
-      sizeCol: 2,
+      sizeRow: 4,
+      sizeCol: 4,
       cells: [],
       isSolved: false,
       hasStarted: false,
@@ -72,27 +72,36 @@ class App extends Component {
       this.randomizeGrid();
       this.setState({hasStarted: true});
     } else if (!this.state.selectedCell) {
-      this.setState({selectedCell: index}, console.log(this.state.selectedCell));
-    } else if (this.state.selectedCell){
-      //slide left
+      this.setState({selectedCell: index});
+    } else if (this.state.selectedCell && this.state.selectedCell === index) {
+      this.setState({selectedCell: null});
+    } else if (this.state.selectedCell && this.state.selectedCell !== index){
       if (
         cells[index - 1] &&
-        (index - 1) === this.state.selectedCell
+        (index - 1) === this.state.selectedCell &&
+        index % sizeCol !==0
       ) {
         this.slideLeft(index);
+        console.log('left');
       } else if (
         cells[index + 1] &&
-        (index + 1) === this.state.selectedCell
+        (index + 1) === this.state.selectedCell &&
+        index % sizeCol !== sizeCol - 1
       ) {
         this.slideRight(index);
+        console.log('right');
       } else if (
-        cells[index - sizeCol]
+        cells[index - sizeCol] &&
+        index - sizeCol === this.state.selectedCell
       ) {
         this.slideUp(index);
+        console.log('up');
       } else if (
-        cells[index + sizeCol]
+        cells[index + sizeCol] &&
+        index + sizeCol === this.state.selectedCell
       ) {
         this.slideDown(index);
+        console.log('down');
       }
       this.setState({selectedCell: null});
     }
@@ -158,153 +167,6 @@ class App extends Component {
   randomizeGrid(){
     console.log('randomizeGrid');
   }
-  // handleClick(index) {
-  //   //if there is a cell above, below, left, or right of clicked cell, with value 0, swapCell
-  //   const cells = this.state.cells;
-  //   const sizeCol = this.state.sizeCol;
-  //   const blankCellValue = this.state.sizeRow * sizeCol;
-  //   if (this.state.hasStarted === false) {
-  //     this.randomizeGrid();
-  //     this.setState({
-  //       hasStarted: true
-  //     });
-  //   } else if (
-  //     cells[index - 1] &&
-  //     index % sizeCol !== 0 &&
-  //     cells[index - 1].value === blankCellValue
-  //   ) {
-  //     this.slideLeft(index);
-  //   } else if (
-  //     cells[index - sizeCol] &&
-  //     cells[index - sizeCol].value === blankCellValue
-  //   ) {
-  //     this.slideUp(index);
-  //   } else if (
-  //     cells[index + 1] &&
-  //     index % sizeCol !== sizeCol - 1 &&
-  //     cells[index + 1].value === blankCellValue
-  //   ) {
-  //     this.slideRight(index);
-  //   } else if (
-  //     cells[index + sizeCol] &&
-  //     cells[index + sizeCol].value === blankCellValue
-  //   ) {
-  //     this.slideDown(index);
-  //   }
-  // }
-  // slideUp(index) {
-  //   const sizeCol = this.state.sizeCol;
-  //   const blankCellValue = this.state.sizeRow * sizeCol;
-  //   const first = this.state.cells.slice(0, index - sizeCol);
-  //   const blankCell = this.state.cells[index - sizeCol];
-  //   const between = this.state.cells.slice(index - sizeCol + 1, index);
-  //   const clickedCell = this.state.cells[index];
-  //   const last = this.state.cells.slice(index + 1);
-  //   const newCells = [
-  //     ...first,
-  //     { blankCell, value: this.state.cells[index].value },
-  //     ...between,
-  //     { clickedCell, value: blankCellValue },
-  //     ...last
-  //   ];
-  //   this.setState({ cells: newCells }, this.winCheck);
-  // }
-  // slideDown(index) {
-  //   const sizeCol = this.state.sizeCol;
-  //   const blankCellValue = this.state.sizeRow * sizeCol;
-  //   const first = this.state.cells.slice(0, index);
-  //   const clickedCell = this.state.cells[index];
-  //   const between = this.state.cells.slice(index + 1, index + sizeCol);
-  //   const blankCell = this.state.cells[index + sizeCol];
-  //   const last = this.state.cells.slice(index + sizeCol + 1);
-  //   const newCells = [
-  //     ...first,
-  //     { clickedCell, value: blankCellValue },
-  //     ...between,
-  //     { blankCell, value: this.state.cells[index].value },
-  //     ...last
-  //   ];
-  //   this.setState({ cells: newCells }, this.winCheck);
-  // }
-  // slideRight(index) {
-  //   const blankCellValue = this.state.sizeRow * this.state.sizeCol;
-  //   const first = this.state.cells.slice(0, index);
-  //   const clickedCell = this.state.cells[index];
-  //   const blankCell = this.state.cells[index + 1];
-  //   const last = this.state.cells.slice(index + 2);
-  //   const newCells = [
-  //     ...first,
-  //     { clickedCell, value: blankCellValue },
-  //     { blankCell, value: this.state.cells[index].value },
-  //     ...last
-  //   ];
-  //   this.setState({ cells: newCells }, this.winCheck);
-  // }
-  // slideLeft(index) {
-  //   const blankCellValue = this.state.sizeRow * this.state.sizeCol;
-  //   const clickedCell = this.state.cells[index];
-  //   const blankCell = this.state.cells[index - 1];
-  //   const first = this.state.cells.slice(0, index - 1);
-  //   const last = this.state.cells.slice(index + 1);
-  //   const newCells = [
-  //     ...first,
-  //     { blankCell, value: this.state.cells[index].value },
-  //     { clickedCell, value: blankCellValue },
-  //     ...last
-  //   ];
-  //   this.setState({ cells: newCells }, this.winCheck);
-  // }
-  // randomizeGrid() {
-  //   const sizeCol = this.state.sizeCol;
-  //   const gridSize = sizeCol * this.state.sizeRow;
-  //   let randomCellArray = this.state.cells;
-  //   let emptyCellIndex = randomCellArray.findIndex(
-  //     item => item.value === gridSize
-  //   );
-  //   for (let i = 0; i < gridSize * 100; i += 1) {
-  //     const randomSlide = Math.floor(Math.random() * 4);
-  //     if (
-  //       randomSlide === 0 &&
-  //       emptyCellIndex % sizeCol !== sizeCol - 1 &&
-  //       randomCellArray[emptyCellIndex + 1]
-  //     ) {
-  //       const temp = randomCellArray[emptyCellIndex];
-  //       randomCellArray[emptyCellIndex] = randomCellArray[emptyCellIndex + 1];
-  //       randomCellArray[emptyCellIndex + 1] = temp;
-  //       emptyCellIndex += 1;
-  //     } else if (
-  //       randomSlide === 1 &&
-  //       this.state.cells[emptyCellIndex + sizeCol]
-  //     ) {
-  //       const temp = randomCellArray[emptyCellIndex];
-  //       randomCellArray[emptyCellIndex] =
-  //         randomCellArray[emptyCellIndex + sizeCol];
-  //       randomCellArray[emptyCellIndex + sizeCol] = temp;
-  //       emptyCellIndex += sizeCol;
-  //     } else if (
-  //       randomSlide === 2 &&
-  //       emptyCellIndex % sizeCol !== 0 &&
-  //       this.state.cells[emptyCellIndex - 1]
-  //     ) {
-  //       const temp = randomCellArray[emptyCellIndex];
-  //       randomCellArray[emptyCellIndex] = randomCellArray[emptyCellIndex - 1];
-  //       randomCellArray[emptyCellIndex - 1] = temp;
-  //       emptyCellIndex -= 1;
-  //     } else if (
-  //       randomSlide === 3 &&
-  //       this.state.cells[emptyCellIndex - sizeCol]
-  //     ) {
-  //       const temp = randomCellArray[emptyCellIndex];
-  //       randomCellArray[emptyCellIndex] =
-  //         randomCellArray[emptyCellIndex - sizeCol];
-  //       randomCellArray[emptyCellIndex - sizeCol] = temp;
-  //       emptyCellIndex -= sizeCol;
-  //     }
-  //   }
-  //   this.setState({
-  //     cells: randomCellArray
-  //   });
-  // }
   winCheck() {
     const winCheckArray = this.state.cells.map(cell => cell.value - 1);
     if (winCheckArray.every((index, element) => index === element)) {
