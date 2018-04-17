cb8e88367daf51c03cdfc422ba3dbc69bd260b34
Cody Selman <codyleeselman@gmail.com>
2018-04-16 23:03:16 -0400

    Filled out content for how to play section. Added styles to HowToPlay component.

--- client/src/HowToPlay.js
+++ client/src/HowToPlay.js
@@ -3,8 +3,14 @@ import './HowToPlay.css';
 
 const HowToPlay = props => {
     return(
-        <div>
-            helo
+        <div className='HowToPlay'>
+            <h1>how to play:</h1>
+            <p>Click on the puzzle to begin. Rearrange the tiles back to their original position before the timer runs out!
+                Swap tiles by clicking on a tile to select it and then clicking on an adjacent tile to swap their positions. 
+                Alternatively, you can play with the keyboard by moving the selector around with the arrow keys, 
+                selecting the tile you want to swap with ENTER, and then pressing up, down, left, or right to swap the 
+                selected tile with an adhacent tile.
+            </p>
         </div>
     );
 };
