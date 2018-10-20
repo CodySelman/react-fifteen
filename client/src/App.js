0e8f39ba5d0a556d74ef33b43db578cddabd25ad
CodySelman-SS <cody.selman@sharpspring.com>
2018-10-20 17:43:26 -0400

    Rearranged methods in App.js for clarity

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
       timeRemaining: 30000,
       gameOver: false
     };
+    // this binds
     this.randomizeGrid = this.randomizeGrid.bind(this);
     this.winCheck = this.winCheck.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -38,46 +39,21 @@ class App extends Component {
     this.timer = this.timer.bind(this);
     this.gameStart = this.gameStart.bind(this);
   }
+
+  // initializing game
   componentDidMount() {
     this.gameStart();
   }
-
-  //gamecontroller methods
   gameStart() {
     Promise.resolve()
+      .then(this.stopTimer())
       .then(this.initializeState())
       .then(this.changeImage())
       .catch(err => console.log(err));
   }
-  startPuzzle() {
-    this.randomizeGrid();
-    this.startTimer();
+  stopTimer() {
+    clearInterval(this.state.timerId);
   }
-  nextLevel() {
-    Promise.resolve()
-      .then(
-        this.setState({
-          isLoading: true,
-          hasStarted: false,
-          viewingFullImage: false,
-          timeRemaining: 0
-        })
-      )
-      .then(this.changeDifficulty())
-      .then(this.changeImage())
-      .then(this.changeGridSize())
-      .then(this.setTimer())
-      .catch(err => console.log(err));
-  }
-  gameOver() {
-    if (this.state.timeRemaining <= 0) {
-      this.setState({
-        gameOver: true,
-        hasStarted: false
-      });
-    }
-  }
-
   initializeState() {
     this.setState({
       ...this.state,
@@ -99,6 +75,134 @@ class App extends Component {
       gameOver: false
     });
   }
+  changeImage() {
+    const currentPics = this.state.currentPics;
+    const randomImageIndex = Math.floor(Math.random() * currentPics.length);
+    const randomImage = currentPics[randomImageIndex];
+    const img = new Image();
+    img.onload = () => {
+      const newImage = {
+        url: randomImage
+      };
+      this.setState(
+        {
+          currentImage: newImage,
+          isSolved: false
+        },
+        this.setState({ isLoading: false })
+      );
+    };
+    img.src = randomImage;
+  }
+
+  // beginning game round
+  startPuzzle() {
+    this.randomizeGrid();
+    this.startTimer();
+  }
+  randomizeGrid() {
+    //Fisher-Yates Shuffle
+    const cells = [...this.state.cells];
+    let currentIndex = cells.length,
+      temporaryValue,
+      randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = cells[currentIndex];
+      cells[currentIndex] = cells[randomIndex];
+      cells[randomIndex] = temporaryValue;
+    }
+    this.setState({ cells: cells });
+  }
+  startTimer() {
+    const timerId = setInterval(this.timer, 10);
+    this.setState({ timerId: timerId });
+  }
+  timer() {
+    const newTimeRemaining = this.state.timeRemaining - 10;
+    if (newTimeRemaining >= 0) {
+      this.setState({ timeRemaining: newTimeRemaining });
+    } else {
+      this.stopTimer();
+      this.gameOver();
+    }
+  }
+
+  // handling end of round
+  winCheck() {
+    const winCheckArray = this.state.cells.map(cell => cell.value - 1);
+    if (winCheckArray.every((index, element) => index === element)) {
+      this.stopTimer();
+      this.setState(
+        {
+          isSolved: true,
+          viewingFullImage: true
+        },
+        this.updateScore()
+      );
+    }
+  }
+  updateScore() {
+    const score = this.state.score;
+    const levelScore =
+      this.state.sizeCol * this.state.sizeRow +
+      Math.floor(this.state.timeRemaining / 5000);
+    const newScore = score + levelScore;
+    this.setState({ score: newScore });
+  }
+  nextLevel() {
+    Promise.resolve()
+      .then(
+        this.setState({
+          isLoading: true,
+          hasStarted: false,
+          viewingFullImage: false,
+          timeRemaining: 0
+        })
+      )
+      .then(this.changeDifficulty())
+      .then(this.changeImage())
+      .then(this.changeGridSize())
+      .then(this.setTimer())
+      .catch(err => console.log(err));
+  }
+  changeDifficulty() {
+    const score = this.state.score;
+    if (score < 50) {
+      this.setState({ currentPics: easyPics });
+    } else if (score >= 50 && score < 100) {
+      this.setState({ currentPics: intermediatePics });
+    } else if (score >= 100) {
+      this.setState({ currentPics: hardPics });
+    }
+  }
+  changeGridSize() {
+    const sizeCol = this.state.sizeCol;
+    const sizeRow = this.state.sizeRow;
+    if (sizeRow + sizeCol < 10) {
+      if (sizeCol > sizeRow) {
+        this.setState(
+          {
+            sizeRow: sizeRow + 1
+          },
+          this.createCells
+        );
+      } else {
+        this.setState(
+          {
+            sizeCol: sizeCol + 1
+          },
+          this.createCells
+        );
+      }
+    }
+  }
+  setTimer() {
+    const timeRemaining = this.state.timeRemaining;
+    const newTimeRemaining = timeRemaining + 30000;
+    this.setState({ timeRemaining: newTimeRemaining });
+  }
   createCells() {
     const newCells = [];
     const gridSize = this.state.sizeCol * this.state.sizeRow;
@@ -110,6 +214,16 @@ class App extends Component {
       cells: newCells
     });
   }
+  gameOver() {
+    if (this.state.timeRemaining <= 0) {
+      this.setState({
+        gameOver: true,
+        hasStarted: false
+      });
+    }
+  }
+  
+  // tile logic
   handleClick(index) {
     const cells = this.state.cells;
     const sizeCol = this.state.sizeCol;
@@ -154,7 +268,6 @@ class App extends Component {
       this.setState({ selectedCellIndex: null });
     }
   }
-
   slideUp(index) {
     const selectedCellIndex = this.state.selectedCellIndex;
     const first = this.state.cells.slice(0, selectedCellIndex);
@@ -213,113 +326,7 @@ class App extends Component {
     ];
     this.setState({ cells: newCells }, this.winCheck);
   }
-  randomizeGrid() {
-    //Fisher-Yates Shuffle
-    const cells = [...this.state.cells];
-    let currentIndex = cells.length,
-      temporaryValue,
-      randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = cells[currentIndex];
-      cells[currentIndex] = cells[randomIndex];
-      cells[randomIndex] = temporaryValue;
-    }
-    this.setState({ cells: cells });
-  }
-  winCheck() {
-    const winCheckArray = this.state.cells.map(cell => cell.value - 1);
-    if (winCheckArray.every((index, element) => index === element)) {
-      this.stopTimer();
-      this.setState(
-        {
-          isSolved: true,
-          viewingFullImage: true
-        },
-        this.updateScore()
-      );
-    }
-  }
-  updateScore() {
-    const score = this.state.score;
-    const levelScore =
-      this.state.sizeCol * this.state.sizeRow +
-      Math.floor(this.state.timeRemaining / 5000);
-    const newScore = score + levelScore;
-    this.setState({ score: newScore });
-  }
-  changeDifficulty() {
-    const score = this.state.score;
-    if (score < 50) {
-      this.setState({ currentPics: easyPics });
-    } else if (score >= 50 && score < 100) {
-      this.setState({ currentPics: intermediatePics });
-    } else if (score >= 100) {
-      this.setState({ currentPics: hardPics });
-    }
-  }
-  changeImage() {
-    const currentPics = this.state.currentPics;
-    const randomImageIndex = Math.floor(Math.random() * currentPics.length);
-    const randomImage = currentPics[randomImageIndex];
-    const img = new Image();
-    img.onload = () => {
-      const newImage = {
-        url: randomImage
-      };
-      this.setState(
-        {
-          currentImage: newImage,
-          isSolved: false
-        },
-        this.setState({ isLoading: false })
-      );
-    };
-    img.src = randomImage;
-  }
-  changeGridSize() {
-    const sizeCol = this.state.sizeCol;
-    const sizeRow = this.state.sizeRow;
-    if (sizeRow + sizeCol < 10) {
-      if (sizeCol > sizeRow) {
-        this.setState(
-          {
-            sizeRow: sizeRow + 1
-          },
-          this.createCells
-        );
-      } else {
-        this.setState(
-          {
-            sizeCol: sizeCol + 1
-          },
-          this.createCells
-        );
-      }
-    }
-  }
-  startTimer() {
-    const timerId = setInterval(this.timer, 10);
-    this.setState({ timerId: timerId });
-  }
-  timer() {
-    const newTimeRemaining = this.state.timeRemaining - 10;
-    if (newTimeRemaining >= 0) {
-      this.setState({ timeRemaining: newTimeRemaining });
-    } else {
-      this.stopTimer();
-      this.gameOver();
-    }
-  }
-  stopTimer() {
-    clearInterval(this.state.timerId);
-  }
-  setTimer() {
-    const timeRemaining = this.state.timeRemaining;
-    const newTimeRemaining = timeRemaining + 30000;
-    this.setState({ timeRemaining: newTimeRemaining });
-  }
+  
   render() {
     return (
       <div>
